04434b6198f232a8238272f1d08222f3885711e5
Steve Morris <77394121+SteveMorris12@users.noreply.github.com>
2022-12-13 23:14:47 +0000

    actual file

---
+++ Apology/open_link.py
@@ -0,0 +1,8 @@
+# Import the webbrowser module
+import webbrowser
+
+# Define the link to navigate to
+link = "https://youtu.be/Ydn-QTMW4YQ"
+
+# Open the web browser and navigate to the link
+webbrowser.open(link)
